4bd713a672dc4598b0cd5a17f7c814154d14aad2
Eric Lee <toptaldev123@gmail.com>
2022-03-05 19:32:06 -0800

    configure react

---
+++ apps/lisk-react/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      {/* <NxWelcome title="lisk-react" /> */}
+      <div className="bg-black text-yellow-300">This is tailwind component</div>
+    </>
+  );
+}
+
+export default App;

8af4af0a03ba035b2470d1da593daaba78f81b67
Eric Lee <toptaldev123@gmail.com>
2022-03-05 19:45:03 -0800

    feat: install tailwind

---
+++ apps/lisk-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lisktest-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'lisk-angular';
+}
